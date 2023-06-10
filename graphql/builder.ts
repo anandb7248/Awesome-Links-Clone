4b575953ff4f6e8d968cf12de960aeba390de649
anandb7248 <anandb7248@gmail.com>
2023-06-09 21:21:00 -0700

    apollo client query with relay and pagination

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -3,20 +3,18 @@ import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
 import prisma from "../lib/prisma";
+import RelayPlugin from '@pothos/plugin-relay';
 
-// 2. 
 export const builder = new SchemaBuilder<{
-  // 3. 
   PrismaTypes: PrismaTypes
 }>({
-  // 4.
-  plugins: [PrismaPlugin],
+  plugins: [PrismaPlugin, RelayPlugin],
+  relayOptions: {},
   prisma: {
     client: prisma,
   }
 })
 
-// 5. 
 builder.queryType({
   fields: (t) => ({
     ok: t.boolean({
